894396e67583631392dabb7dccbbc2b0a81cf5ac
lifeparticle <mahbubzaman17@gmail.com>
2021-07-29 21:52:52 +1000

    refact

--- build_readme.py
+++ build_readme.py
@@ -2,12 +2,17 @@ from datetime import datetime as date
 from PIL import ImageFont
 from PIL import ImageDraw
 from PIL import Image
-import numpy as np
 
-if __name__ == "__main__":
-	img = Image.open("bg.png")
-	draw = ImageDraw.Draw(img)
+def load_bg_image(image_name):
+	return Image.open(image_name)
+
+def output_final_image(output_image_name, bg_image):
+	draw = ImageDraw.Draw(bg_image)
 	font = ImageFont.truetype("Roboto-Light.ttf", 16)
 	draw.rectangle((530, 80, 640, 130), outline = None, fill = 'black')
 	draw.text((550, 95), date.today().strftime("%A"), (255, 255, 255), font = font)
-	img.save('output.png')
+	bg_image.save(output_image_name)
+
+if __name__ == "__main__":
+	bg_image = load_bg_image("bg.png")
+	output_final_image("output.png", bg_image)
